c2596b996b52980bac34a1b5935bad0f305ce548
rahulrainaaa <rahulrainahome@live.com>
2022-03-14 12:27:16 +0530

    Initial Commit.

--- app/src/main/java/app/calculator/running/MainActivity.kt
+++ app/src/main/java/app/calculator/running/MainActivity.kt
@@ -3,17 +3,19 @@ package app.calculator.running
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.navigation.compose.rememberNavController
 import app.calculator.running.ui.theme.RunningCalculatorTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RunningCalculatorTheme {
                 val navController = rememberNavController()
-                MainScreen(navController)
+                MainScreen(navController = navController, mainVM = mainViewModel)
             }
         }
     }
